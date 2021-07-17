aff6996aa0e096af11d76f7e5a29a66e1c73b8ec
ahandal24 <61199977+ahandal24@users.noreply.github.com>
2021-07-17 00:07:07 -0400

    -Added collection view -Added tab bar controller to switch between screens

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Amber Handal on 7/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
